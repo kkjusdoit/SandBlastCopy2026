a42e3273752f7f9e23795f65f950e1de7a4aea12
linkunkun <linkunkun@secretlisa.com>
2026-07-11 17:21:27 +0800

    style(gameplay): increase soft drop speed by 30% to 4x (0.25f interval)

--- Assets/Scripts/FlowSand/Runtime/FlowSandMatchCoordinator.cs
+++ Assets/Scripts/FlowSand/Runtime/FlowSandMatchCoordinator.cs
@@ -176,7 +176,7 @@ namespace FlowSand.Runtime
             float stepInterval = GetCurrentDropInterval();
             if (softDropHeld)
             {
-                stepInterval *= 0.33f;
+                stepInterval *= 0.25f;
             }
 
             while (pieceFallTimer >= stepInterval)
